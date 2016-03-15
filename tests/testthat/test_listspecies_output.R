752dc7ee082dff3724b6b181dd8732e9abb6c204
wirawara <wirawara@users.noreply.github.com>
2016-03-15 22:52:30 +0100

    update

--- tests/testthat/test_listspecies_output.R
+++ tests/testthat/test_listspecies_output.R
@@ -1,10 +1,8 @@
 context("expect_output")
 
 test_that("Distinct species", {
-
-	species <- listBgeeSpecies()
-
-  	expect_that( species, is_a("list") )
-    expect_that( length(species), equals(2) )
-    expect_that( unique(names(species) %in% c("rna_seq", "affymetrix")), is_true() )
+  species <- listBgeeSpecies()
+	expect_that( species, is_a("list") )
+  expect_that( length(species), equals(2) )
+  expect_that( unique(names(species) %in% c("rna_seq", "affymetrix")), is_true() )
 })
