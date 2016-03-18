ec7148686d5e8d841e6d278d8587555c95f9cf20
Julien Roux <julien.roux@unil.ch>
2016-03-18 11:48:31 +0100

    Added chck on input data to makeTable.R

--- R/makeTable.R
+++ R/makeTable.R
@@ -1,34 +1,42 @@
 ########################
-#' @title Formats the results obtained from gene set enrichment test on anatomical structures
+#' @title Formats results of the enrichment test on anatomical structures.
 #'
-#' @description This function loads the results from the test function (topGO package) and creates an output table with organ names, fold enrichment and FDR
+#' @description This function loads the results from the topGO test and creates an output table with organ names, fold enrichment and FDR. Data are sorted by p-value and only terms below the specified FDR cutoff are included.
 #'
 #' @param topAnatData A list produced by the function loadTopAnatData().
 #'
 #' @param topAnatObject An object produced by the function topAnat().
 #'
-#' @param results A result object, produced for example by the runtest() function of topGO
+#' @param results A result object, produced by the runtest() function of topGO.
 #'
-#' @param cutoff An FDR cutoff between 0 and 1. Only terms with FDR lower than this cutoff will be in the output. Default is 1.
+#' @param cutoff An FDR cutoff between 0 and 1. Only terms with FDR lower than this cutoff are included. Default is 1, meaning that all terms are included.
 #'
-#' @return A data frame with significantly enriched anatomical structures, sorted by FDR
+#' @return A data frame with significantly enriched anatomical structures, sorted by p-value.
 #'
 #' @author Julien Roux \email{julien.roux@unil.ch}.
 #'
 #' @examples
 #'   \dontrun{
-#'     ## Launch topGO test on ata loaded from Bgee
+#'     ## Launch topGO test on data loaded from Bgee
 #'     resFis <- runTest(myTopAnatObject, algorithm = 'elim', statistic = 'fisher')
 #'     tableOver <- makeTable(myTopAnatData, myTopAnatObject, resFis, 0.1)
 #'   }
 #' @export
 
-makeTable <- function(topAnatData, topAnatObject, results, cutoff){
-  ## TO DO: check input objects
-  ## score(results)
-  ## is.numeric(cutoff)
-  ## termStat(topAnatObject, ...)
-  ## topAnatData$organ.names not empty (see topAnat.R)
+makeTable <- function(topAnatData, topAnatObject, results, cutoff=1){
+  ## Perform some checks on the input data
+  if(is.na(score(results)) | length(score(results)) == 0){
+    stop("Problem: the results object is empty.")
+  }
+  if (!is.numeric(cutoff)){
+    cutoff <- as.numeric(cutoff)
+  }
+  if(is.null(myTopAnatObject)){
+    stop("Problem: the topAnatObject is empty.")
+  }
+  if( length(topAnatData$organ.names[,1]) == 0 ) {
+    stop("Problem: the organ.names data frame of your topAnatData object is empty.")
+  }
 
   ## retrieve p-values for the enrichment
   scores <- score(results)
@@ -37,7 +45,7 @@ makeTable <- function(topAnatData, topAnatObject, results, cutoff){
   topTerms <- as.data.frame(topTerms)
 
   if( nrow(topTerms) != 0 ){
-    cat("\nBuilding the results table for the", nrow(topTerms), "significant terms at FDR threshold of", cutoff, "... ")
+    cat(paste0("\nBuilding the results table for the ", nrow(topTerms), " significant terms at FDR threshold of ", cutoff, "... "))
     odds <- termStat(topAnatObject, row.names(topTerms))
     foldEnrichment <- odds[2]/odds[3]
 
