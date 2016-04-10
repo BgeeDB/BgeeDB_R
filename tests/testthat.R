fcbad01909d7530e858691895f3b7c8ac73af4b2
Andrea Komljenovic <andreakomljenovic@gmail.com>
2016-04-10 20:12:20 +0000

    DESCIRPTION
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BgeeDB@116085 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat.R
+++
@@ -1,5 +0,0 @@
-library(testthat)
-library(BgeeDB)
-
-Sys.setenv("R_TESTS" = "")
-test_check("BgeeDB")
--- tests/testthat/test_listspecies_output.R
+++
@@ -1,9 +0,0 @@
-context("expect_output")
-
-test_that("Distinct species", {
-  species <- listBgeeSpecies()
-	expect_that( species, is_a("list") )
-  expect_that( length(species), equals(3) )
-  expect_message(message("Last update:"))
-  expect_true( unique(names(species) %in% c("Species in the Database", "RNAseq", "Affymetrix")) )
-})
