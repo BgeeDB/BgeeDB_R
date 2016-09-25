c0f2fc00e44207f47ff191147492f3337d625d5f
wirawara <andrea.komljenovic@sib.swiss>
2016-09-26 00:33:09 +0200

    update testlistspecies_output

--- tests/testthat/test_listspecies_output.R
+++ tests/testthat/test_listspecies_output.R
@@ -2,8 +2,11 @@ context("expect_output")
 
 test_that("Distinct species", {
   species <- listBgeeSpecies()
-	expect_that( species, is_a("list") )
-  expect_that( length(species), equals(3) )
-  expect_message(message("Last update:"))
-  expect_true( unique(names(species) %in% c("Species in the Database", "RNAseq", "Affymetrix")) )
+	expect_that( species, is_a("data.frame") )
+  expect_that( ncol(species), equals(8) )
+  expect_message(message("Querying Bgee to get release information..."))
+  expect_true( colnames(species) %in% c("ID", "GENUS",
+                                        "SPECIES_NAME", "COMMON_NAME",
+                                        "AFFYMETRIX", "EST",
+                                        "IN_SITU", "RNA_SEQ"))
 })
