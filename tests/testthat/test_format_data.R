084b76b5cd1985469606389e8a8eda50e68f7d38
Julien Roux <julien.roux@unil.ch>
2016-09-27 11:58:00 +0200

    Fixed problems in test units

--- tests/testthat/test_format_data.R
+++ tests/testthat/test_format_data.R
@@ -4,9 +4,7 @@ context("expect_output")
 test_that("Formatting gene expression files", {
   bgee <- Bgee$new(species = "Mus_musculus", datatype = "rna_seq")
   data_bgee_mouse_exp <- bgee$get_data(experiment.id  = "GSE30617")
-  gene.expression <- bgee$format_data(data_bgee_mouse_exp, calltype = "expressed", stats = "rpkm")
+  gene.expression <- bgee$format_data(data_bgee_mouse_exp, calltype = "present", stats = "rpkm")
 
-  # expect_that( gene.expression, is_a("list") )
   expect_true(class(gene.expression) == "ExpressionSet" )
-
 })
--- tests/testthat/test_listspecies_output.R
+++ tests/testthat/test_listspecies_output.R
@@ -4,9 +4,9 @@ test_that("Distinct species", {
   species <- listBgeeSpecies()
 	expect_that( species, is_a("data.frame") )
   expect_that( ncol(species), equals(8) )
-  expect_message(message("Querying Bgee to get release information..."))
-  expect_true( colnames(species) %in% c("ID", "GENUS",
-                                        "SPECIES_NAME", "COMMON_NAME",
-                                        "AFFYMETRIX", "EST",
-                                        "IN_SITU", "RNA_SEQ"))
+  expect_message(message("Query to Bgee webservice successful!"))
+  expect_true( all(colnames(species) %in% c("ID", "GENUS",
+                                            "SPECIES_NAME", "COMMON_NAME",
+                                            "AFFYMETRIX", "EST",
+                                            "IN_SITU", "RNA_SEQ")))
 })
