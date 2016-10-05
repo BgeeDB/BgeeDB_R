1031d72c190535973637e036f782c724b15e7153
Julien Roux <julien.roux@unil.ch>
2016-10-05 14:59:31 +0200

    Renamed listRelease.R to listBgeeRelease.R

---
+++ R/listBgeeRelease.R
@@ -0,0 +1,47 @@
+#' @title List Bgee releases available to use with BgeeDB package
+#'
+#' @description Returns information on available Bgee releases, the access URL for FTP and webservice, and the date of release
+#'
+#' @param release A character specifying a targeted release number. In the form "Release.subrelease" or "Release_subrelease", e.g., "13.2" or 13_2". If not specified, all available releases are shown.
+#'
+#' @return A data frame with information on Bgee releases.
+#'
+#' @examples{
+#'  listBgeeRelease()
+#' }
+#'
+#' @author Julien Roux
+#' @export
+
+# Function displaying the user a data frame describing all releases available for Bgee
+listBgeeRelease <- function(release=NULL){
+  cat("Downloading release information from Bgee...\n")
+  allReleases <- .getBgeeRelease()
+  if (length(release)==1){
+    if (sum(allReleases$release == gsub("_", ".", release))==1){
+      cat(paste0("Only displaying information from targeted release ",
+                 gsub("_", ".", release), "\n"))
+      allReleases <- allReleases[allReleases$release == gsub("_", ".", release), ]
+    } else {
+      stop("ERROR: The specified release number is invalid or is not available for BgeeDB.")
+    }
+  }
+  ## Only return the columns of interest to the user
+  return(allReleases[, c("release","release.date", "FTP.URL", "TopAnat.URL")])
+}
+
+# Function returning a data frame describing all releases available for Bgee
+.getBgeeRelease <- function(){
+  ## query FTP to get file describing all releases
+  releaseurl <- 'ftp://ftp.bgee.org/release.tsv'
+  try(download.file(releaseurl, quiet = TRUE,
+                    destfile=file.path(getwd(), 'release.tsv')
+  ),
+  silent=FALSE)
+  if (!file.exists("release.tsv")){
+    stop("ERROR: File describing releases could not be downloaded from FTP.")
+  }
+  allReleases <- read.table("release.tsv", header=TRUE, sep="\t")
+  file.remove(file.path(getwd(), 'release.tsv'))
+  return(allReleases)
+}
