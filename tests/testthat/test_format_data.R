4dacde55b71d40487be2b7de6814292050f46068
Julien Wollbrett <julien.wollbrett@unil.ch>
2024-09-05 16:29:18 +0200

    correct a test on fpkm for Bgee 15.2 as it is not used anymore

--- tests/testthat/test_format_data.R
+++ tests/testthat/test_format_data.R
@@ -4,12 +4,13 @@ context("expect_output")
 test_that("Formatting gene expression files", {
   bgee <- Bgee$new(species = "Mus_musculus", dataType = "rna_seq")
   data_bgee_mouse_exp <- getData(bgee, experimentId  = "GSE43721")
-  gene.expression <- formatData(bgee, data_bgee_mouse_exp, callType = "present", stats = "fpkm")
-  expect_true(class(gene.expression) == "ExpressionSet" )
   gene.expression <- formatData(bgee, data_bgee_mouse_exp, callType = "present", stats = "tpm")
   expect_true(class(gene.expression) == "ExpressionSet" )
   gene.expression <- formatData(bgee, data_bgee_mouse_exp, callType = "present", stats = "counts")
   expect_true(class(gene.expression) == "ExpressionSet" )
+  # not available since bgee_v15_1
+  expect_error(formatData(bgee, data_bgee_mouse_exp, callType = "present", stats = "fpkm"),
+               "Choose whether data formatting should create a matrix of TPMs or read counts, with stats option set as \"tpm\" or \"counts\"")
   # not available since bgee_v14
   expect_error(formatData(bgee, data_bgee_mouse_exp, callType = "present", stats = "rpkm"), 
                "Choose whether data formatting should create a matrix of FPKMs, TPMs or read counts, with stats option set as \"fpkm\", \"tpm\" or \"counts\"")
